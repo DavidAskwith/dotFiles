d2a65362f717b2d8463c7ce666fcd5b87d324b2b
David Askwith <dkaskwith@hotmail.com>
2017-02-13 19:45:14 -0500

    comment cleanup and .urxvt removal

--- init.sh
+++ init.sh
@@ -21,38 +21,19 @@ rm -rv /home/dave/.i3
 rm -v /home/dave/.gitconfig
 rm -v /home/dave/.Xresources
 rm -v /home/dave/.fonts
-rm -v /home/dave/.urxvt
 
 #------install section-----#
 
 #creating symbolic links for . folders
 
-#.vimrc
 ln -sv /home/dave/.dotFiles/vimrc /home/dave/.vimrc
-
-#.vim
 ln -sv /home/dave/.dotFiles/vim /home/dave/.vim
-
-#.bashrc
 ln -sv /home/dave/.dotFiles/bashrc /home/dave/.bashrc
-
-#.bash_aliases
 ln -sv /home/dave/.dotFiles/bash_aliases /home/dave/.bash_aliases
-
-#.gitconfig
 ln -sv /home/dave/.dotFiles/gitconfig /home/dave/.gitconfig
-
-#.i3
 ln -sv /home/dave/.dotFiles/i3 /home/dave/.i3
-
-#.Xresources
 ln -sv /home/dave/.dotFiles/Xresources /home/dave/.Xresources
-
-#.fonts
 ln -sv /home/dave/.dotFiles/fonts /home/dave/.fonts
-
-#.urxvt
-ln -sv /home/dave/.dotFiles/urxvt /home/dave/.urxvt
 
 #adds vundle to vim folder
 sudo -u dave git clone https://github.com/VundleVim/Vundle.vim.git /home/dave/.dotFiles/vim/bundle/Vundle.vim
