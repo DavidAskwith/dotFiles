93f8e39b2b0bfe5fbee9ace19f653fcfa1bf1502
David Askwith <dkaskwith@hotmail.com>
2017-09-25 13:24:43 -0400

    node, gtop and blueman installs added

--- scripts/coreApps.sh
+++ scripts/coreApps.sh
@@ -10,8 +10,14 @@ apt-get --assume-yes install chromium
 #ntfs-3g for working with ntfs
 apt-get --assume-yes install ntfs-3g
 
+#----BlueTooth and Wi-Fi----#
+
 #wireless networking
 apt-get --assume-yes install firmware-iwlwifi network-manager network-manager-gnome
+
+#bluetooth
+apt-get -assume-yes install bluedevil blueman pulseaudio-module-bluetooth
+
 
 #TODO: create a print statement that sets up the network config
 #rm /etc/NetworkManager/NetworkManager.conf
@@ -23,11 +29,21 @@ apt-get --force-yes --assume-yes install unzip zip
 #adds apt feature such as add-apt-repository
 apt-get --assume-yes install software-properties-common
 
-#libreoffice and package for gtk theme suport
+#libreoffice and package for gtk theme support
 apt-get --assume-yes --force-yes install libreoffice libreoffice-gtk
 
 #installs alsa pulseaudio pavucontrol to control sound alsa-utils gets you amixer for volume control
 apt-get --assume-yes install pulseaudio pavucontrol alsa-utils
+
+#installs nodejs
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+apt-get --assume-yes install nodejs
+
+#Graphical version of htop for system monitoring
+npm install -g gtop
+
+#insatlls ranger file explorer
+apt-get --assume-yes install ranger 
 
 #TODO: test below
 #microsoft fonts
